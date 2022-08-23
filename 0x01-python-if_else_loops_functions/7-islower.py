479494deac4736f1fb2115600c2c67e909ba3e13
Yoseph Mekonnen <yosephgod@gmail.com>
2022-08-23 14:00:23 -0700

    Task 7

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 def islower(c):
-    if ord(c) > 64 and ord(c) < 91:
-        return False
-    return True
+    if ord(c) > 96 and ord(c) < 123:
+        return True
+    return False
