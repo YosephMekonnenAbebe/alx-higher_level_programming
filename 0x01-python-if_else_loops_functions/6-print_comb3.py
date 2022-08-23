f38d2f67c53f0aeb68928ce4c7c00c874213837c
Yoseph Mekonnen <yosephgod@gmail.com>
2022-08-23 13:24:30 -0700

    Task 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+colon = ", "
+for yoseph in range(0, 100):
+    Last = yoseph % 10
+    First = (int(yoseph/10))
+    if yoseph < 10:
+        print("{}{}".format(0, yoseph), end=colon)
+    if yoseph > 9:
+        if (Last == First) or (Last == 0) or (First > Last):
+            continue
+        else:
+            if yoseph == 89:
+                colon = "\n"
+        print(yoseph, end=colon)
