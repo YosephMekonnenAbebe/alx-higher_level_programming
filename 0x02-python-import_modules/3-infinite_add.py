61aa5d9d8152ee5df8304042ea81d9db83a857b8
YosephMekonnenAbebe <yosephgod@gmail.com>
2022-08-26 00:00:26 +0300

    Task 3

---
+++ 0x02-python-import_modules/3-infinite_add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    suma = 0
+    yoseph = 0
+    for a in sys.argv:
+        if yoseph > 0:
+            suma += int(a)
+        i += 1
+    print(suma)
