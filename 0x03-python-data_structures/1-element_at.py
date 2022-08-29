3b9b0474960a775997729a63b6597175a0c719e2
Yoseph Mekonnen <yosephgod@gmail.com>
2022-08-29 03:01:58 -0700

    Task 1

--- 0x03-python-data_structures/1-element_at.py
+++ 0x03-python-data_structures/1-element_at.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def element_at(my_list, idx):
-    listyoseph =  len(my_list) - 1
-     if (idx < 0 or idx > listyoseph):
-         return (None)
-     else:
-         return (my_list[idx])
+    yosephlist = len(my_list) - 1
+    if (idx < 0 or idx > yosephlist):
+        return (None)
+    else:
+        return (my_list[idx])
