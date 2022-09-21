a796b791cd37df59ad21e4cd73b68bca7f94201e
YosephMekonnenAbebe <yosephgod@gmail.com>
2022-09-21 15:08:49 +0300

    Tasks All

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""""This module create a rectangle side by side"""
+
+
+class Rectangle:
+    """
+    pass atribute - empty class
+    """
+    def __init__(self):
+        pass
